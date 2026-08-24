07e7010ec788814608abc9d49901a2994a3971b6
DANNYPY <olatundedaniel943@gmail.com>
2026-08-24 22:44:51 +0100

    refactor(layout): make main layout to happen in the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { useScreen } from "@/lib/ScreenProvider";
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
+import { ScreenProvider, useScreen } from "@/lib/ScreenProvider";
 import Assignments from "@/lib/screens/Assignment";
 import Courses from "@/lib/screens/Courses";
 import Dashboard from "@/lib/screens/Dashboard";
@@ -8,6 +10,18 @@ import LiveClasses from "@/lib/screens/LiveClasses";
 import Image from "next/image";
 
 export default function Home() {
+  return (
+    <ScreenProvider>
+      <Sidebar />
+      <div className="flex-1">
+        <Header />
+        <HomeTree />
+      </div>
+    </ScreenProvider>
+  );
+}
+
+function HomeTree() {
   const { activeScreen } = useScreen();
   return (
     <main className="flex-1 overflow-auto py-7 md:px-8 lg:px-10">
