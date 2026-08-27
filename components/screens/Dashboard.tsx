eb9a6d13a305943783bec9f3254b35f856efc1f6
DANNYPY <olatundedaniel943@gmail.com>
2026-08-27 13:37:22 +0100

    style(dashboard): adjust dashboard grid breakpoints

--- components/screens/Dashboard.tsx
+++ components/screens/Dashboard.tsx
@@ -168,7 +168,7 @@ export default function Dashboard() {
       </div>
 
       {/* metricItems */}
-      <div className="grid gap-3 sm:grid-cols-2 xl:grid-cols-4">
+      <div className="grid gap-3 grid-cols-2 lg:grid-cols-4">
         {metricItems.map(({ Icon, label, value, growth }) => (
           <div
             key={label}
